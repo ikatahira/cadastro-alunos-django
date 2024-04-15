904a998421531681e258fdaf06973c901586127d
ikatahira <isaque.katahira@fatec.sp.gov.br>
2024-04-15 02:30:38 +0000

    duckdb e django

---
+++ alunos/forms.py
@@ -0,0 +1,9 @@
+# alunos/forms.py
+
+from django import forms
+from .models import Aluno
+
+class AlunoForm(forms.ModelForm):
+    class Meta:
+        model = Aluno
+        fields = ['nome', 'matricula']
--- alunos/views.py
+++ alunos/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# alunos/views.py
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import AlunoForm
+from .models import Aluno
+
+def cadastrar_aluno(request):
+    if request.method == 'POST':
+        form = AlunoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = AlunoForm()
+    return render(request, 'alunos/cadastrar_aluno.html', {'form': form})
