47390a85264b55fe7eb5be22be1c482b9fc5c980
ikatahira <isaque.katahira@fatec.sp.gov.br>
2024-04-15 03:13:48 +0000

    salvando

--- alunos/views.py
+++ alunos/views.py
@@ -2,14 +2,13 @@
 
 from django.shortcuts import render, redirect
 from .forms import AlunoForm
-from .models import Aluno
 
 def cadastrar_aluno(request):
     if request.method == 'POST':
         form = AlunoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return redirect('index')  # Redirecionar para a página inicial após o cadastro
     else:
         form = AlunoForm()
-    return render(request, 'alunos/cadastrar_aluno.html', {'form': form})
+    return render(request, 'cadastrar_aluno.html', {'form': form})
